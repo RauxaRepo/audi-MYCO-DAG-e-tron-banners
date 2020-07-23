fef248c48630ef116241ef183af162979bf92e03
Joshua S. Pacheco <jpacheco@rauxa.com>
2020-07-23 11:57:09 -0400

    adding local copy overlay image population and animation on end-frame

--- 2020_MYCO_T2_160x600_LOWER_etron/js/script.js
+++ 2020_MYCO_T2_160x600_LOWER_etron/js/script.js
@@ -21,7 +21,8 @@ var imageArray = new Array(),
     secondFrame = false,
     thirdFrame = false,
     efSwitch = false,
-    checkTimer;
+    checkTimer,
+    localCopy;
 
 var outer = document.getElementById("outer"),
     outer_background = outer.getElementsByClassName("background")[0],
@@ -41,7 +42,8 @@ var outer = document.getElementById("outer"),
     inner_headline = inner.getElementsByClassName("headline")[0],
     inner_headlineText = inner.getElementsByClassName("headline")[0],
     mLock = document.getElementsByClassName("modelLockup")[0],
-    endFrame_headCont = inner.getElementsByClassName("efHeadline_container")[0];
+    endFrame_headCont = inner.getElementsByClassName("efHeadline_container")[0],
+    localCopy = document.getElementById("headline_local_copy");
 
 var hoverCheck = false,
     disclaimerOne = false,
@@ -430,6 +432,7 @@ function animateFrameThree(){
             },"end");
             // mLock = document.createElement("img");
             mLock.src = "etron_K_1x6.png";
+            localCopy.src = "2020_Audietron_Lower_LC_160x600.png";
             replay.addEventListener("click", resetAll);
             // mLock.classList.add("modelLockup");
             // main.appendChild(mLock);
@@ -444,6 +447,7 @@ function animateFrameThree(){
                 cta.style.opacity = "1";
                 replay.style.display = "block";
                 replay.style.opacity = "1";
+                localCopy.style.opacity = "1";
                 }
             })
     },1370);
@@ -521,6 +525,7 @@ function resetAll() {
             resetElement(cta);
             resetElement(inner_headline);
             resetElement(replay);
+            resetElement(localCopy);
             TweenMax.set(mLock, {opacity:0,x:148});
             resetElement(disclaimer);
             thirdFrame = false;
